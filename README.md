b2e13450565ca020e32daeb8c15535ac5a76f7dc
Andy Fragen <andy@thefragens.com>
2014-07-18 09:01:09 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+wp-theme-tweaks
+================
+
+Base plugin to add tweaks to help style your WP theme. Sometimes it's easier not to add stuff to a child theme.
---
+++ wp-theme-tweaks.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name:       WP Theme Tweaks
+Plugin URI:        https://github.com/afragen/wp-theme-tweaks
+Description:       Theme tweaks for your WP site that are not included in your theme.
+Author:            Andy Fragen
+Version:           0.1.0
+Author URI:        http://thefragens.com
+GitHub Plugin URI: https://github.com/afragen/wp-theme-tweaks
+GitHub Branch:     master
+*/
+
+//add extra css without needing to create child theme
+add_action( 'wp_enqueue_scripts', 'wptt_add_xtras_css', 99 );
+function wptt_add_xtras_css() {
+	wp_enqueue_style( 'wp-theme-tweaks' , plugins_url( 'wp-theme-tweaks.css', __FILE__ ) );
+}
