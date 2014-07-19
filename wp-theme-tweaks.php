9b76c019122d98a588c5c551b691d292055a1629
Andy Fragen <andy@thefragens.com>
2014-07-19 11:26:14 -0700

    initial commit thefragens

--- wp-theme-tweaks.php
+++ wp-theme-tweaks.php
@@ -7,11 +7,23 @@ Author:            Andy Fragen
 Version:           0.1.0
 Author URI:        http://thefragens.com
 GitHub Plugin URI: https://github.com/afragen/wp-theme-tweaks
-GitHub Branch:     master
+GitHub Branch:     thefragens
 */
 
 //add extra css without needing to create child theme
 add_action( 'wp_enqueue_scripts', 'wptt_add_xtras_css', 99 );
 function wptt_add_xtras_css() {
 	wp_enqueue_style( 'wp-theme-tweaks' , plugins_url( 'wp-theme-tweaks.css', __FILE__ ) );
+}
+
+add_action( 'admin_menu', 'ajf_remove_menu_pages' );
+function ajf_remove_menu_pages() {
+	remove_menu_page( 'link-manager.php' );
+}
+
+//http://www.paulund.co.uk/automatically-link-twitter
+add_filter( 'the_content', 'paulund_content_twitter_mention' );   
+add_filter( 'comment_text', 'paulund_content_twitter_mention' );
+function paulund_content_twitter_mention( $content ) {
+	return preg_replace( '/([^a-zA-Z0-9-_&])@([0-9a-zA-Z_]+)/', "$1<a href=\"http://twitter.com/$2\" target=\"_blank\" rel=\"nofollow\">@$2</a>", $content );
 }
