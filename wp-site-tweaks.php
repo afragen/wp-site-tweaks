0c7bec6be49ea49a15794931a2734c14eb1e0948
Andy Fragen <andy@thefragens.com>
2020-05-14 12:06:22 -0700

    oops, do this properly

--- wp-site-tweaks.php
+++ wp-site-tweaks.php
@@ -57,10 +57,10 @@ function sk_show_featured_image_single_posts() {
 }
 
 // remove query strings from static resources. This ensures that they are cached like other elements.
-add_filter( 'script_loader_src', 'ewp_remove_script_version', 15, 1 );
-add_filter( 'style_loader_src', 'ewp_remove_script_version', 15, 1 );
+// add_filter( 'script_loader_src', 'ewp_remove_script_version', 15, 1 );
+// add_filter( 'style_loader_src', 'ewp_remove_script_version', 15, 1 );
 function ewp_remove_script_version( $src ) {
-	// return remove_query_arg( 'ver', $src );
+	return remove_query_arg( 'ver', $src );
 }
 
 add_filter(
